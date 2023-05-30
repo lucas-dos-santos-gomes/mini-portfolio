cdd1ed13485addb3c9bd77b5604791b1d8665776
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-30 03:35:56 -0300

    Modificando variáveis CSS pelo JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,24 @@
 x = Math.random();
-if (x < 0.5) {
-    const head = document.querySelector("head");
-    head.removeChild(head.children[8]);
+if (x < 1) {
+    // const head = document.querySelector("head");
+    // head.removeChild(head.children[8]);
+    /*const body = document.querySelector("body");
+    const cartao = document.querySelector(".cartao");
+    const fotoPerfil = document.querySelector(".minhas-informacoes .foto-perfil");
+    const verdeAzul = document.querySelector(".verde-azul");
+    const descricao = document.querySelector(".descricao");
+    body.style.backgroundImage = "url(./src/img/fundo-programming.jpg)";
+    body.style.backgroundPosition = "center";
+    cartao.style.borderColor = "red";
+    fotoPerfil.style.borderColor = "#02a7d0";
+    verdeAzul.style.color = "#02a7d0";
+    descricao.style.webkitScrollbarThumb = "red";
+    console.log(descricao.style);*/
+    document.documentElement.style.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
+    document.documentElement.style.setProperty('--bg-pst', "center");
+    document.documentElement.style.setProperty('--rosa', "red");
+    document.documentElement.style.setProperty('--verde-azul', "#02a7d0");
+
 }
 
 const abas = document.querySelectorAll(".aba");
