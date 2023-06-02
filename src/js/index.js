8976574994356655ca61b906a85d45d3999a7209
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 02:57:50 -0300

    Adicionando função de troca de tela com duplo clique

--- src/js/index.js
+++ src/js/index.js
@@ -9,21 +9,35 @@ if (random < 0.5) {
   styleState = true;
 }
 
+function alterScreen() {
+  if(styleState) {
+    styleRoot.setProperty('--img-url', "url(../img/fundo-neon-city.gif)");
+    styleRoot.setProperty('--bg-pst', "center top");
+    styleRoot.setProperty('--rosa', "#e228d8");
+    styleRoot.setProperty('--verde-azul', "#02d08f");
+    styleState = false;
+  } else {
+    styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
+    styleRoot.setProperty('--bg-pst', "center");
+    styleRoot.setProperty('--rosa', "red");
+    styleRoot.setProperty('--verde-azul', "#02a7d0");
+    styleState = true;
+  }
+}
+
+let touches = [-100, -100];
+
+window.addEventListener("touchend", e => {
+  touches.push(e.timeStamp);
+  touches.shift();
+  if(touches[1] - touches[0] < 300) {
+    alterScreen();
+  }
+});
+
 window.addEventListener("keypress", e => {
   if(e.code == "Space") {
-    if(styleState) {
-      styleRoot.setProperty('--img-url', "url(../img/fundo-neon-city.gif)");
-      styleRoot.setProperty('--bg-pst', "center top");
-      styleRoot.setProperty('--rosa', "#e228d8");
-      styleRoot.setProperty('--verde-azul', "#02d08f");
-      styleState = false;
-    } else {
-      styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
-      styleRoot.setProperty('--bg-pst', "center");
-      styleRoot.setProperty('--rosa', "red");
-      styleRoot.setProperty('--verde-azul', "#02a7d0");
-      styleState = true;
-    }
+    alterScreen();
   }
 });
 
