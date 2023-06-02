8f671ecd163e21c093ddfc8c91c72301c52fc19c
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 02:29:16 -0300

    Identando JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,52 @@
 x = Math.random();
 if (x < 0.5) {
-    const styleRoot = document.documentElement.style;
-    styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
-    styleRoot.setProperty('--bg-pst', "center");
-    styleRoot.setProperty('--rosa', "red");
-    styleRoot.setProperty('--verde-azul', "#02a7d0");
+  const styleRoot = document.documentElement.style;
+  styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
+  styleRoot.setProperty('--bg-pst', "center");
+  styleRoot.setProperty('--rosa', "red");
+  styleRoot.setProperty('--verde-azul', "#02a7d0");
 }
 
 const abas = document.querySelectorAll(".aba");
 
 abas.forEach(aba => {
-    aba.addEventListener("click", function(){
-        if (aba.classList.contains("selecionada")) {
-            return;
-        }
-        return selecionarAba(aba);
-    });
+  aba.addEventListener("click", function(){
+    if (aba.classList.contains("selecionada")) {
+      return;
+    }
+    return selecionarAba(aba);
+  });
 });
 
 function selecionarAba(aba) {
-    const abaSelecionada = document.querySelector(".aba.selecionada");
-    abaSelecionada.classList.remove("selecionada");
-    aba.classList.add("selecionada");
+  const abaSelecionada = document.querySelector(".aba.selecionada");
+  abaSelecionada.classList.remove("selecionada");
+  aba.classList.add("selecionada");
 
-    return selecionarInformacao(aba);
+  return selecionarInformacao(aba);
 }
 
 function selecionarInformacao(aba) {
-    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
-    informacaoSelecionada.classList.remove("selecionado");
+  const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+  informacaoSelecionada.classList.remove("selecionado");
 
-    const idInformacao = (`informacao-${aba.id}`);
-    const informacaoMostrada = document.getElementById(idInformacao);
-    informacaoMostrada.classList.add("selecionado");
+  const idInformacao = (`informacao-${aba.id}`);
+  const informacaoMostrada = document.getElementById(idInformacao);
+  informacaoMostrada.classList.add("selecionado");
 }
 
 const linksSociais = document.querySelectorAll(".redes-sociais_links");
 linksSociais.forEach(item => {
-    item.addEventListener("mouseover", () => {
-        for(let i = 0; i < linksSociais.length; i++) {
-            if(linksSociais[i] != item) {
-                linksSociais[i].style.opacity = "0.5";
-            }
-        }
-    });
-    item.addEventListener("mouseout", () => {
-        for(let i = 0; i < linksSociais.length; i++) {
-            linksSociais[i].style.opacity = "1";
-        }
-    });
+  item.addEventListener("mouseover", () => {
+    for(let i = 0; i < linksSociais.length; i++) {
+      if(linksSociais[i] != item) {
+        linksSociais[i].style.opacity = "0.5";
+      }
+    }
+  });
+  item.addEventListener("mouseout", () => {
+    for(let i = 0; i < linksSociais.length; i++) {
+      linksSociais[i].style.opacity = "1";
+    }
+  });
 });
