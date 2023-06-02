916e692cdb70448c86012aa86f8aa9ed55497716
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 02:40:30 -0300

    Adicionando troca de tela com espaço

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,31 @@
-x = Math.random();
-if (x < 0.5) {
-  const styleRoot = document.documentElement.style;
+const styleRoot = document.documentElement.style;
+let styleState = false;
+let random = Math.random();
+if (random < 0.5) {
   styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
   styleRoot.setProperty('--bg-pst', "center");
   styleRoot.setProperty('--rosa', "red");
   styleRoot.setProperty('--verde-azul', "#02a7d0");
+  styleState = true;
 }
+
+window.addEventListener("keypress", e => {
+  if(e.code == "Space") {
+    if(styleState) {
+      styleRoot.setProperty('--img-url', "url(../img/fundo-neon-city.gif)");
+      styleRoot.setProperty('--bg-pst', "center top");
+      styleRoot.setProperty('--rosa', "#e228d8");
+      styleRoot.setProperty('--verde-azul', "#02d08f");
+      styleState = false;
+    } else {
+      styleRoot.setProperty('--img-url', "url(../img/fundo-programming.jpg)");
+      styleRoot.setProperty('--bg-pst', "center");
+      styleRoot.setProperty('--rosa', "red");
+      styleRoot.setProperty('--verde-azul', "#02a7d0");
+      styleState = true;
+    }
+  }
+});
 
 const abas = document.querySelectorAll(".aba");
 
